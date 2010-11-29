73e272a3a0b3838244fbb916c0a01fe3e4fc80db
LekisS <lekiss@gmail.com>
2010-11-29 01:16:05 +0100

    Imported original jelly-auth module

---
+++ classes/model/user/token.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+/**
+ * Model User Token
+ * @package Jelly Auth
+ * @author	Israel Canasa
+ */
+class Model_User_Token extends Model_Auth_User_Token {}
